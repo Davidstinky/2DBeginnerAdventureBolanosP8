9e20e8d36d4fc2cd8133e7dde48a1279a967a5a9
Davidstinky <751474@student.dusd.net>
2024-11-07 15:24:22 -0800

    made progress on my project
    i added damage zones that damage the player and enemies that also damage the players and move around

--- 2DBeginnerAdventureP8Bolanos/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP8Bolanos/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 7.0f;
     public int maxHealth = 5;
+    public float timeInvincible = 2;
     public int health { get { return currentHealth; } }
     int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -25,6 +29,15 @@ public class PlayerController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if(invincibleTimer < 0)
+            {
+                isInvincible = false;
+            }
+        }
+
 
         
     }
@@ -38,6 +51,15 @@ public class PlayerController : MonoBehaviour
 
     public void ChangeHealth(int amount)
     {
+        if(amount < 0)
+        {
+            if(isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
