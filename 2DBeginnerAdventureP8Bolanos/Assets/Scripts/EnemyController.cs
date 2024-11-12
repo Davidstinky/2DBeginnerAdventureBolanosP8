53b5efeb702ceb43844126b7a275c9740d802798
Davidstinky <751474@student.dusd.net>
2024-11-12 15:27:00 -0800

    made progress on my beginner adventure project
    i added animation to my main character and the enemy robot. i also updated my GDP.

--- 2DBeginnerAdventureP8Bolanos/Assets/Scripts/EnemyController.cs
+++ 2DBeginnerAdventureP8Bolanos/Assets/Scripts/EnemyController.cs
@@ -14,11 +14,14 @@ public class EnemyController : MonoBehaviour
     float timer;
     int direction = 1;
 
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2 = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -37,10 +40,14 @@ public class EnemyController : MonoBehaviour
         Vector2 position = rigidbody2.position;
         if(vertical)
         {
+            animator.SetFloat("Move x", 0);
+            animator.SetFloat("Move Y", direction);
             position.y = position.y + Time.deltaTime * speed * direction;
         }
         else
         {
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move y", 0);
             position.x = position.x + Time.deltaTime * speed * direction;
         }
         
